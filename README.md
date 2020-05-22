1dca2d3abedeb740040f04611bb518db14758167
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-05-22 22:57:13 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Want to settle a bet but can't find the dice?
+
+Do it on this webpage!
+
+Coded this as a challenge to test what I learned on DOM manipulation and Javascript.
+
+Download file and open the dicee.html with any browser.
---
+++ index.js
@@ -0,0 +1,36 @@
+function gameStart() {
+  numberGenerator1()
+  numberGenerator2()
+  winner()
+}
+
+
+function numberGenerator1() {
+  var randomNumber1 = (Math.ceil(Math.random() * 6));
+  document.querySelector(".img1").setAttribute("src", ("images/dice" + [randomNumber1] + ".png"));
+
+  return randomNumber1
+}
+
+function numberGenerator2() {
+  var randomNumber2 = (Math.ceil(Math.random() * 6));
+  document.querySelector(".img2").setAttribute("src", "images/dice" + [randomNumber2] + ".png");
+
+  return randomNumber2
+}
+
+function winner() {
+  var player1 = numberGenerator1();
+  var player2 = numberGenerator2();
+
+  if (player1 > player2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins!"
+  }
+  else if (player1 < player2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!"
+  }
+  else {
+    document.querySelector("h1").innerHTML = "It's a Draw!"
+  }
+}
+
